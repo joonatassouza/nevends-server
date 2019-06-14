775402471ac0d3c49f4b44bd867338b4297229b0
Jonatas Souza <jonatas@easycomtec.com>
2019-06-14 15:46:35 -0300

    First commit

---
+++ src/controllers/LikeController.js
@@ -0,0 +1,27 @@
+const Post = require('../models/Post');
+
+module.exports = {
+    async like(req, res) {
+        const post = await Post.findById(req.params.id);
+
+        post.likes++;
+
+        await post.save();
+
+        req.io.emit('like', post);
+
+        return res.json(post);
+    },
+
+    async dislike(req, res) {
+        const post = await Post.findById(req.params.id);
+
+        post.likes--;
+
+        await post.save();
+
+        req.io.emit('like', post);
+
+        return res.json(post);
+    }
+};
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
+const PostController = require('./controllers/PostController');
+const LikeController = require('./controllers/LikeController');
+
+const routes = new express.Router();
+const upload = multer(uploadConfig);
+
+routes.post('/posts', upload.single('image'), PostController.store);
+routes.get('/posts', PostController.index);
+routes.delete('/posts/:id', PostController.delete);
+
+routes.post('/posts/:id/like', LikeController.like);
+routes.post('/posts/:id/dislike', LikeController.dislike);
+
+module.exports = routes;
